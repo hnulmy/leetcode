a238dec0c56ba02d935701bf1e4b6658371515b3
hnulmy <sayeter@live.com>
2021-02-04 20:46:00 +0800

    add demo

---
+++ script/demo.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+class treeNode(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = left
+        self.right = right
+
+class testDemo(object):
+    def __init__(self):
+        self.ip_dict = {}
+
+def main():
+    test_demo = testDemo()
+    print('demo test')
+
+if __name__ == '__main__':
+    main()
---
+++ src/demo.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+class demoTest {
+    public:
+        int position;
+    private:
+        int rank;
+};
+
+int main() {
+    demoTest demo_test;
+    demo_test.position = 0;
+
+    cout << "demo test" << endl;
+    return 0;
+}
