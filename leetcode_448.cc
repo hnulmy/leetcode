cab508b0ff8250456caa9b20c12018695fa79cec
hnulmy <sayeter@live.com>
2021-02-08 15:27:37 +0800

    fix bug

--- leetcode_448.cc
+++ leetcode_448.cc
@@ -8,17 +8,15 @@ using namespace std;
 // Find All  Numbers Disappeared in an Array
 class Solution {
  public:
-   vector<int> FindDisappearedNumbers(vector<int>& nums) {
+   vector<int> findDisappearedNumbers(vector<int>& nums) {
      vector<int> ret_vec;
      for (int i = 0; i < nums.size(); i ++) {
        int current = (nums.at(i) > 0 ? nums.at(i) : -1 * nums.at(i)) - 1;
        nums.at(current) = nums.at(current) > 0 ? -1 * nums.at(current) : nums.at(current);
      }
      for (int i = 0; i < nums.size(); i ++) {
-       //cout << nums[i] << endl;
        if (nums.at(i) > 0) {
          ret_vec.push_back(i + 1);
-         cout << i + 1 << endl;
        }
      }
      return ret_vec;
@@ -26,12 +24,8 @@ class Solution {
 };
 
 int main(int argc, char *argv[]) {
-  cout << argc << endl;
-  cout << *argv << endl;
-  //cout << *(argv+1)[0] << *(argv+2)[0] << endl;
-  cout << *(argv[1]) <<endl << *(argv + 1) <<endl;
-  Solution solution;
   vector<int> vec = {1, 2, 4, 2, 3};
-  solution.FindDisappearedNumbers(vec);
+  Solution solution;
+  solution.findDisappearedNumbers(vec);
   return 0;
 }
