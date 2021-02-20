707f955d843ed81aabe32812bfa86816658bb57e
hnulmy <sayeter@live.com>
2021-02-21 00:59:18 +0800

    fix bug

---
+++ plus_one.cc
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//
+
+class Solution {
+ public:
+  vector<int> plusOne(vector<int>& digits) {
+    vector<int> vec;
+    int index = digits.size() - 1;
+    int number = 10;
+    while (index >= 0) {
+      number = number / 10 + digits[index];
+      if (number == 10) {
+        vec.push_back(0);
+      } else {
+        vec.push_back(number);
+      }
+      index --;
+    }
+    if (vec[digits.size() - 1] == 0) {
+      vec.push_back(1);
+    }
+    vector<int> ret;
+    for (int index = vec.size() - 1; index >= 0; index --) {
+      ret.push_back(vec.at(index));
+    }
+    return ret;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  cout << argc << endl;
+  return 0;
+}
